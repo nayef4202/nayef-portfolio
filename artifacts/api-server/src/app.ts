aaa8041a0a0345f9ca89b64032baac26d8053fa1
nayef4202 <nayef4202@users.noreply.github.com>
2026-09-10 17:08:28 +0300

    Fix tsconfig base interop and simplify app.ts logging middleware

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,37 +1,14 @@
-import express, { type Express } from "express";
+import express, { type Express, type Request, type Response, type NextFunction } from "express";
 import cors from "cors";
-import * as pinoHttpModule from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-const getPinoHttp = (): any => {
-  const mod: any = pinoHttpModule;
-  return mod.pinoHttp || mod.default || mod;
-};
-
-const pinoMiddleware = getPinoHttp();
-
-app.use(
-  pinoMiddleware({
-    logger,
-    serializers: {
-      req: (req: any) => {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res: (res: any) => {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  })
-);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  logger.info({ method: req.method, url: req.url }, "Incoming request");
+  next();
+});
 
 app.use(cors());
 app.use(express.json());
